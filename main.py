b94994c61e6d93272a0565ed8fb68e6a0d42858d
JonCWMusic <jon.chris.whelpley@gmail.com>
2024-01-10 11:44:21 -0500

    Continuing to work at getting thumbnails to display in tkinter window.

--- main.py
+++ main.py
@@ -27,6 +27,36 @@ api_key = config.api_key
 youtube = build('youtube', 'v3', developerKey = api_key)
 
 
+# class to make thumbnail grid within tkinter for a nice display
+class ThumbnailGridApp(object):
+    def __init__(self, master, image_paths):
+        self.master = master
+        self.image_paths = image_paths
+        self.create_thumbnail_grid()
+
+    def create_thumbnail_grid(self):
+        row = 0
+        col = 0
+
+        for path in self.image_paths:
+            thumbnail = self.load_thumbnail(path)
+            thumbnail_label = tk.Label(self.master, image=thumbnail)
+            thumbnail_label.grid(row=row, column=col, padx=5, pady=5)
+
+            col += 1
+            if col == 4:  # You can adjust the number of columns as needed
+                col = 0
+                row += 1
+
+    def load_thumbnail(self, path):
+        original_image = Image.open(path)
+        # Resize the image to create a thumbnail
+        thumbnail_size = (50, 50)
+        thumbnail_image = original_image.resize(thumbnail_size, Image.ANTIALIAS)
+        thumbnail = ImageTk.PhotoImage(thumbnail_image)
+        return thumbnail
+
+
 # Function to convert channel name to channel id
 def get_channel_id(channel_name):
     search_response = youtube.search().list(
@@ -45,32 +75,21 @@ def get_channel_id(channel_name):
         return None
 
 
-# # Function to scrape video titles using Beautiful Soup
-# def scrape_videos_with_bs(channel_url):
-#     response = requests.get(channel_url)
-#     soup = bs4.BeautifulSoup(response.text, 'html.parser')
-#     print(response)
-#     print(soup)
-#     print(soup.select('.yt-simple-endpoint.style-scope.yt-formatted-string'))
-#     video_titles = [title.text for title in soup.select('.yt-simple-endpoint.style-scope.yt-formatted-string')]
-#     print(video_titles)
-#     return video_titles
-
-
 def scrape_videos_with_bs(channel_url):
+    '''
+    :param channel_url: url for the Youtube channel to scrape
+    :return: a tuple with two lists. One containing the video thumbnail Urls and one containing the video titles
+    '''
     response = requests.get(channel_url)
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     pattern = re.compile(r'ytInitialData')
+
     # title and thumbnail array init
     video_titles = []
     video_thumbnails = []
-    # print(soup.prettify())
-    # site_json = json.loads(soup.text)
-    # print(site_json)
+
     # search for a script tag within html using BS4
     script_tag = soup.find("script", string = pattern)
-    # print(script_tag.string)
-    # print(script_tag.string.index('ytInitialData = ') + 16)
     json_text_from_channel = script_tag.string[script_tag.string.index('ytInitialData = ') + 16:len(script_tag.string)-1]
 
 
@@ -81,10 +100,7 @@ def scrape_videos_with_bs(channel_url):
         # Parse the JSON data
         json_data_from_channel = json.loads(json_text_from_channel)
         # Navigate through the JSON structure to get the thumbnails
-        # iterate over the index on contents
-        print(json_data_from_channel['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['richGridRenderer']['contents'])
-        print(len(json_data_from_channel['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content'][
-                  'richGridRenderer']['contents']))
+        # iterate over the index on contents with list comprehension
         video_thumbnails = [json_data_from_channel['contents']['twoColumnBrowseResultsRenderer'][
                           'tabs'][1]['tabRenderer']['content']['richGridRenderer']['contents'][
                           i]['richItemRenderer']['content']['videoRenderer'][
@@ -94,14 +110,14 @@ def scrape_videos_with_bs(channel_url):
                           'tabs'][1]['tabRenderer']['content']['richGridRenderer']['contents'][i]['richItemRenderer']['content'][
                 'videoRenderer']['title']['runs'][0]['text'] for i in range(0, len(json_data_from_channel['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['richGridRenderer']['contents'])-1)]
 
-
-            # video_titles.append(title)
-            # video_thumbnails.append(get_thumbnail(item))
-
     return video_titles, video_thumbnails
 
 # Function to retrieve video titles using YouTube API
 def get_videos_with_api(channel_id):
+    '''
+    :param channel_id: channel id taken from the api search
+    :return: video titles from the api call
+    '''
     playlist_response = youtube.channels().list(
         part='contentDetails',
         id=channel_id
@@ -112,33 +128,17 @@ def get_videos_with_api(channel_id):
     playlist_items = youtube.playlistItems().list(
         part='snippet',
         playlistId=uploads_playlist_id,
-        maxResults=10  # Adjust as needed
+        maxResults=30  # Adjust as needed. Max is 50.
     ).execute()
 
     video_titles = [item['snippet']['title'] for item in playlist_items['items']]
     return video_titles
 
-
-# Function to retrieve thumbnails from videos
-def get_thumbnail(url):
-    response = requests.get(url)
+def get_images_from_url(image_url):
+    response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
-    img.thumbnail((100, 100))  # Resize thumbnail if needed
-    thumbnail = ImageTk.PhotoImage(img)
-    return thumbnail
-
-
-def get_thumbnail_url(video_title):
-    thumbnail_url = f'http://img.youtube.com/vi/{video_title}/maxresdefault.jpg'
-    return thumbnail_url
-
-
-# Function to actually display each thumbnail in the tkinter application
-def display_thumbnails(thumbnails):
-    for i, thumbnail in enumerate(thumbnails):
-        thumbnail_label = ttk.Label(thumbnails_frame, image=thumbnail)
-        thumbnail_label.grid(row=i // 3, column=i % 3, padx=10, pady=10)
-
+    # img.thumbnail((100, 100))  # Resize thumbnail if needed
+    return img
 
 # Function for the button click event
 def on_button_click():
@@ -149,21 +149,30 @@ def on_button_click():
     channel_url = f'https://www.youtube.com/c/{channel_string}/videos'
     if channel_id:
         bs_video_titles = scrape_videos_with_bs(channel_url)[0]
-        #bs_result_label.config(text=f'Video Titles (Beautiful Soup): {", ".join(bs_video_titles)}')
+        bs_thumbnail_urls = scrape_videos_with_bs(channel_url)[1]
+
         bs_result_text.delete(1.0, tk.END)  # Clear existing content
         bs_result_text.insert(tk.END, '\n'.join(bs_video_titles))
 
         # Retrieve video titles using YouTube API
         api_video_titles = get_videos_with_api(channel_id)
-        #api_result_label.config(text=f'Video Titles (YouTube API): {", ".join(api_video_titles)}')
         api_result_text.delete(1.0, tk.END)  # Clear existing content
         api_result_text.insert(tk.END, '\n'.join(api_video_titles))
+
+        thumbnail_url_text.delete(1.0, tk.END)
+        thumbnail_url_text.insert(tk.END, '\n'.join(bs_thumbnail_urls))
+
+        # thumbnail_images = [tk.PhotoImage(,image = get_images_from_url(item)) for item in bs_thumbnail_urls]
+
     else:
         bs_result_text.delete(1.0, tk.END)
         bs_result_text.insert(tk.END, 'Channel not found')
 
         api_result_text.delete(1.0, tk.END)
         api_result_text.insert(tk.END, 'Channel not found')
+
+        thumbnail_url_text.delete(1.0, tk.END)
+        thumbnail_url_text.insert(tk.END, 'Channel not found')
 
 
 if __name__ == '__main__':
@@ -180,24 +189,21 @@ if __name__ == '__main__':
     button = ttk.Button(root, text='Get Video Titles', command=on_button_click)
     button.grid(row=0, column=2, padx=10, pady=10)
 
-    # bs_result_label = ttk.Label(root, text='Video Titles (Beautiful Soup):')
-    # bs_result_label.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-    #
-    # api_result_label = ttk.Label(root, text='Video Titles (YouTube API):')
-    # api_result_label.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
-
+    # notebook object is what contains the tab groups
     notebook = ttk.Notebook(root)
     notebook.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
     # Create tabs
-    tab_titles = ['Beautiful Soup', 'YouTube API', 'Thumbnails']
+    # tab_titles = ['Beautiful Soup', 'YouTube API', 'Thumbnails', 'Thumbnail URLs']
     bs_tab = ttk.Frame(notebook)
     api_tab = ttk.Frame(notebook)
     thumbnails_tab = ttk.Frame(notebook)
+    thumbnail_urls = ttk.Frame(notebook)
 
     notebook.add(bs_tab, text='Beautiful Soup')
     notebook.add(api_tab, text='YouTube API')
     notebook.add(thumbnails_tab, text='Thumbnails')
+    notebook.add(thumbnail_urls, text = 'Thumbnail URLs')
 
     # Text widget for Beautiful Soup result
     bs_result_text = tk.Text(bs_tab, height=10, width=40)
@@ -207,9 +213,19 @@ if __name__ == '__main__':
     api_result_text = tk.Text(api_tab, height=10, width=40)
     api_result_text.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
 
+    # Text Widget for Thumbnail URLs
+    thumbnail_url_text = tk.Text(thumbnail_urls, height=10, width= 40)
+    thumbnail_url_text.grid(row = 3, column = 0, columnspan =  3, padx = 10, pady = 10)
+
+    # Images from thumbnails
+    # thumbnail_images = tk.PhotoImage(image = thumbnail_urls)
+    # thumbnail_images = []
+    # Display it within a label.
+    # label = ttk.Label(image=thumbnail_images)
     # Frame for displaying thumbnails
     thumbnails_frame = ttk.Frame(thumbnails_tab)
     thumbnails_frame.grid(row=0, column=0, padx=10, pady=10)
 
     # Start the Tkinter event loop
     root.mainloop()
+
